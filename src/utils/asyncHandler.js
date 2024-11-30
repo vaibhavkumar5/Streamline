1cb9b4eb3806e08a5d508dedce4ccd9928c1d2a7
Vaibhav Kumar <mrkumarvaibhav2001@gmail.com>
2024-12-01 01:12:49 +0530

    api response and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// Promise syntax
+
+
+const asyncHandler = ( requestHandler) =>{
+     (req, res, next) =>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+     }
+
+}
+
+export {asyncHandler}
+
+
+
+// Try - Catch syntax
+/*
+const asyncHandler=(fn)=> async(req, res, next) => {
+    try {
+        await fn (req, res, next)
+        
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
